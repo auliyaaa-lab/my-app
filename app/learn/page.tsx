1379b72ba3f556da55986ae27abee6d389a04597
unknown <agustinauliya766@gmail.com>
2026-04-20 13:10:58 +0800

    first commit

---
+++ app/learn/page.tsx
@@ -0,0 +1,18 @@
+export default function learnpage() {
+    return (
+        <div>
+            <div>
+                <img src="/palette.svg" alt="Design" />
+                    <p>design</p>
+            </div>
+            <div>
+                <p/>figma
+                <p>8 hours ago</p>
+            </div>
+        </div>
+
+
+    )
+
+}
+
