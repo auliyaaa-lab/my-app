8acb81378818e831ad685bd37bff06c3a46e3655
unknown <agustinauliya766@gmail.com>
2026-04-27 12:29:23 +0800

    update css

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,18 +1,44 @@
 export default function learnpage() {
     return (
-        <div>
-            <div>
+        <div className= "h-screen bg-gray-100 flex gap-4">
+        <div className="bg-purple-300 p-8 h-60 w-70 rounded-4xl flex flex-col justify-between text-gray-900">
+            <div className="flex gap-4 ">
                 <img src="/palette.svg" alt="Design" />
                     <p>design</p>
             </div>
             <div>
-                <p/>figma
+                <p className="text-4xl">figma</p>
                 <p>8 hours ago</p>
             </div>
         </div>
 
 
+        <div className="bg-orange-300 p-8 h-60 w-70 rounded-4xl flex flex-col justify-between text-gray-900">
+            <div className="flex gap-4 ">
+                <img src="/palette.svg" alt="Design" />
+                    <p>Codding</p>
+            </div>
+            <div>
+                <p className="text-4xl">Python</p>
+                <p>2 days ago</p>
+            </div>
+        </div>
+
+
+        <div className="bg-red-300 p-8 h-60 w-70 rounded-4xl flex flex-col justify-between text-gray-900">
+            <div className="flex gap-4 ">
+                <img src="/palette.svg" alt="Design" />
+                    <p>design</p>
+            </div>
+            <div>
+                <p className="text-4xl">Sketch</p>
+                <p>4 days ago</p>
+            </div>
+        </div>
+        </div>
+
     )
 
 }
 
+ 
